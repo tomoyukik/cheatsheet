5550ebde8dfb0294ed75769973edf547b4512713
tomoyukik <kobori.tomoyuki@gmail.com>
2021-01-29 08:21:07 +0900

    add: django cheat cheet

---
+++ cheat_django.py
@@ -0,0 +1,14 @@
+# Django cheat
+
+## カレントディレクトリでプロジェクト作成
+
+```
+mkdir sample_project
+cd sample_project
+django-admin startproject sample_project .
+```
+
+```
+django-admin startproject sample_project
+```
+
