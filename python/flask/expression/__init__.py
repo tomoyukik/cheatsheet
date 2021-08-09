7f382b654a77a2c97752e61ebfac386c160f21c5
tomoyukik <kobori.tomoyuki@gmail.com>
2021-08-09 11:26:55 +0900

    update: rename flask app module name
    - modified:   expression/__init__.py
    - renamed:    expression/server.py -> expression/app.py
    - modified:   main.py

--- python/flask/expression/__init__.py
+++ python/flask/expression/__init__.py
@@ -1,5 +1,2 @@
-# from . import expressions
-from .kazu import Kazu 
-# from .server import app
-from .server import app
-print('sss----------------------------------------------')
+from .kazu import Kazu
+from .app import app
--- python/flask/main.py
+++ python/flask/main.py
@@ -1,36 +1,5 @@
-# %%
-# from expression.server import app
 from expression import app
-
-# from expression import app
-# import flask
-# from werkzeug.wrappers import request
-# from expression import Kazu
-# import logging
-# 
-# # %%
-# app = flask.Flask(__name__)
-# 
-# @app.route('/abc')
-# def cde():
-#     return '123'
-# 
-# @app.route('/<exp>', methods=['GET'])
-# def calc(exp):
-#     nm = flask.request.args.to_dict()
-#     if exp == 'add':
-#         # return str(5)
-#         return str(Kazu(int(nm['n'])).add(int(nm['m'])).to_int())
-#     if exp == 'sabtract':
-#         # return str(10)
-#         return str(Kazu(int(nm['n'])).sabtract(int(nm['m'])).to_int())
-#     return str(111)
-# 
-# @app.route('/dic')
-# def dic():
-#     return {'one': 1, 'two': 2}
 
 if __name__ == '__main__':
     print('ddddddddd')
     app.run('127.0.0.1', port=3334, debug=True )
-# %%
